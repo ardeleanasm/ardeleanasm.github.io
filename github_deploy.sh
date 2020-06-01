78bcc553fb6ab0fc8bc9934fc877bd4273738f4c
ardeleanasm <ardeleanasm@gmail.com>
2020-06-01 16:01:59 +0300

    fixed

--- github_deploy.sh
+++ github_deploy.sh
@@ -1,23 +1,29 @@
 #!/usr/bin/env sh
-#set -euo pipefail
 
-SRC_BRANCH=source
-TGT_BRANCH=master
 
-STATUS="$(git status --porcelain)"
-if [ -n "$STATUS" ]; then
-	echo "Uncommitted changes"
-	echo "$STATUS"
-	exit
-fi
+# temporarily store uncommited changes
+git stash
 
-git checkout $SRC_BRANCH
-MSG=$(git log -n 1 --format="format:%h %f")
-git checkout $TGT_BRANCH
-rm -rf *
-cp -r $RESULT/* .
-chmod u+w . -R
-git add .
-git commit -m "$MSG"
-git push
-git checkout $SRC_BRANCH
+# checkout correct branch
+git checkout source
+
+#build new files
+
+#get previous files
+git fetch --all
+git checkout -b master --track origin/master
+
+source ./build.sh
+
+#overwrite
+cp -a output/. .
+rm -r output
+
+git add -A
+git commit -m "Publish."
+
+git push origin master:master
+
+git checkout source
+git branch -D master
+git stash pop
